253294051e2d9e529e722114545688857a78b137
Josh <josh@gdovindesigns.com>
2021-11-01 19:10:13 -0400

    added demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,9 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(GSDevTools);
+
+const tl = gsap.timeline({repeat:3});
+tl.to(".dog",{duration:0.1, stagger:-0.1,ease: "steps(6)", alpha:0})
+
+GSDevTools.create(); 
