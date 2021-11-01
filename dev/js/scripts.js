3cb4ed5d30886d93a8d5d9eac6631d637b4b796b
Josh <josh@gdovindesigns.com>
2021-11-01 18:24:23 -0400

    demo added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,11 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+
+const tl = gsap.timeline();
+tl.from("#jar-line",{duration:1, drawSVG:"0%"});
+tl.fromTo("#jar-line",{drawSVG:"0% 100%"},{duration:1, drawSVG:"100% 100%"});
+
+GSDevTools.create();
