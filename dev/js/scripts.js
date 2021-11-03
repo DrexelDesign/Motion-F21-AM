e175808f9295a448f2e1c01109728343629fefba
Josh <josh@gdovindesigns.com>
2021-11-03 11:02:14 -0400

    clip path complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,26 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+gsap.set("#clipPath-2",{transformOrigin:"center"});
+
+const tl = gsap.timeline();
+// ---------------------------------------
+//              Demo 1
+// ---------------------------------------
+// tl.from("#boxes",{duration:2,x:"-=600"});
+
+
+// ---------------------------------------
+//              Demo 2
+// ---------------------------------------
+// get the width of the wave-1
+var wave = document.querySelector("#wave-1");
+var wave2 = document.querySelector("#wave-2");
+
+var bBoxGroup = wave.getBBox();
+var bBoxGroup2 = wave2.getBBox();
+
+// move wave 1
+tl.to("#wave-1",{duration:1, x: -bBoxGroup.width / 2, ease: "none", repeat:2},"start")
+tl.to("#wave-2",{duration:2, x: bBoxGroup2.width / 2, ease: "none", repeat:1},"start")
+tl.to("#wave-1",{duration:4, y:"-=200"},"start")
+tl.to("#wave-2",{duration:4, y:"-=200"},"start")
+// tl.to("#clipPath-2",{duration:2, rotation:180},"start");
